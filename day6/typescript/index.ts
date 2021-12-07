fc8937a5359074eb988fd0192b2df820d4e51c01
Ted Kulp <ted@tedkulp.com>
2021-12-07 16:48:53 -0500

    day 6 pt 2

--- day6/typescript/index.ts
+++ day6/typescript/index.ts
@@ -1,35 +1,39 @@
 import { promises } from 'fs';
+import { clone, countBy, sum } from 'lodash';
 
-const DATA_FILE = './day6/test.txt';
+const DATA_FILE = './day6/data.txt';
 const NUM_DAYS_1 = 80;
 const NUM_DAYS_2 = 256;
 
-const calculateFish = (dataList: number[], days: number) => {
-  for (let day = 0; day < days; day++) {
-    dataList = dataList.map((n) => n - 1);
-    if (day + 1 >= days) break;
+type fishCounts = {
+  [daysUntilSplit: string]: number;
+};
 
-    const zeroCount = dataList.filter((n) => n === 0).length;
-    dataList = dataList.filter((n) => n !== 0);
-    if (zeroCount > 0) {
-      dataList = [
-        ...dataList,
-        ...new Array<number>(zeroCount).fill(7),
-        ...new Array<number>(zeroCount).fill(9),
-      ];
+const calculateFish = (dataList: fishCounts, days: number) => {
+  for (let dayNum = 0; dayNum < days; dayNum++) {
+    // Move zeros to 7 and new offspring to 9
+    const zeroCount = dataList['0'] || 0;
+    dataList['7'] = (dataList['7'] || 0) + zeroCount;
+    dataList['9'] = zeroCount;
+
+    // Do the subtraction
+    for (let i = 0; i < 10; i++) {
+      dataList[i.toString()] = dataList[(i + 1).toString()] || 0;
     }
   }
 
-  return dataList.length;
+  return sum(Object.values(dataList));
 };
 
 (async () => {
-  const dataList: number[] = (await promises.readFile(DATA_FILE))
+  const initialData: number[] = (await promises.readFile(DATA_FILE))
     .toString()
     .split(',')
     .filter((n) => !!n)
     .map((n) => parseInt(n, 10));
 
-  console.log(`Answer #1: ${calculateFish(dataList, NUM_DAYS_1)}`);
-  console.log(`Answer #2: ${calculateFish(dataList, NUM_DAYS_2)}`);
+  const dataList: fishCounts = countBy(initialData);
+
+  console.log(`Answer #1: ${calculateFish(clone(dataList), NUM_DAYS_1)}`);
+  console.log(`Answer #2: ${calculateFish(clone(dataList), NUM_DAYS_2)}`);
 })();
